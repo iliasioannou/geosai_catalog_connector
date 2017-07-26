72a87673470e8f6b61059f8344ccab6fa20b285b
Ilias Ioannou <ioannou@planetek.gr>
2017-07-26 17:18:43 +0200

    updated script for admin password

--- datavolume/db.sql
+++ datavolume/db.sql
@@ -2,3 +2,4 @@
 -- Update database settings
 --
 UPDATE settings SET value='CMEMS' WHERE name = 'system/site/name';
+UPDATE users SET password='03479128c2c480a4085299d42896dd11ede6a4ecd213df1805edb8f2040f06ec89579599892be3f1' WHERE name = 'admin';
