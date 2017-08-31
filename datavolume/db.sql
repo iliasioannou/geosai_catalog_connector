c58e29697cc2faa094dbc3b2ae2aa9f10c0c084c
Maurizio Cosmai <Cosmai@planetek.it>
2017-08-31 17:54:52 +0200

    Aggiornato db.sql

--- datavolume/db.sql
+++ datavolume/db.sql
@@ -1,5 +1,5 @@
 --
 -- Update database settings
 --
-UPDATE settings SET value='CMEMS' WHERE name = 'system/site/name';
+UPDATE settings SET value='MARINE' WHERE name = 'system/site/name';
 UPDATE users SET password='03479128c2c480a4085299d42896dd11ede6a4ecd213df1805edb8f2040f06ec89579599892be3f1' WHERE name = 'admin';
